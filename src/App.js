14cb480fcb1b2318700bc19cd78da8d50a52f00c
utkarsh <98549357+utkarsh0428@users.noreply.github.com>
2022-02-02 00:05:49 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -3,28 +3,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   HashRouter as Router,
   Switch,
-  // Route,
+  Route,
 } from "react-router-dom";
 
 import NavBarC from "./Components/NavBarC"
-// import NewsCardCont from './Components/NewsCardCont';
-// import Subscription from './Components/Subscription';
+import NewsCardCont from './Components/NewsCardCont';
+import Subscription from './Components/Subscription';
 
 
 function App() {
   return (
     <>
-    <h1>hi </h1>
     <Router>
       <NavBarC/>
       <Switch>
-        {/* <Route exact path="/"><NewsCardCont category="general"/></Route>
+        <Route exact path="/"><NewsCardCont category="general"/></Route>
         <Route exact path="/business"><NewsCardCont category="business"/></Route>
         <Route exact path="/entertainment"><NewsCardCont category="entertainment"/></Route>
         <Route exact path="/sports"><NewsCardCont category="sports"/></Route>
         <Route exact path="/scienceandtechnology"><NewsCardCont category="science"/></Route>
         <Route exact path="/health"><NewsCardCont category="health"/></Route>
-        <Route exact path="/subscription"><Subscription /></Route> */}
+        <Route exact path="/subscription"><Subscription /></Route>
       </Switch>
     </Router>
     
